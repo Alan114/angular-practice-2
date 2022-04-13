0b829666e3dc9bbab2421b844ba510d2bcff4456
Alan Pecenkovic <alanquad@yahoo.com>
2022-04-13 11:55:54 +0200

    Redirecting and setting up routing from app.ts component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,11 @@ const routes: Routes = [
   { path: 'third', component: ThirdComponent },
   { path: 'fourth', component: FourthComponent },
   {
+    path: '',
+    redirectTo: '/first',
+    pathMatch: 'full',
+  },
+  {
     path: '**',
     component: FirstComponent,
   },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'myapp2';
+  constructor(private router: Router) {}
+
+  goToRoute(route: string = '/fourth'): void {
+    this.router.navigateByUrl(route);
+  }
 }
